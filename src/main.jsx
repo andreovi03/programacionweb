1d9862743209b31c72441fb4b411ddc47dd77ed0
Andrea Oviedo <andreaoviedor@gmail.com>
2024-02-01 19:07:39 -0500

    Header, Boddy, Footer act

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from './App2'
+/*When you have problems with the import, change the name pls*/
+
+const appContainer = document.getElementById("app")
+const root = createRoot(appContainer)
+
+
+/*Cammel case for the component name (starts with uppercase)*/
+
+root.render(<App/>)
+console.log(appContainer)
+
