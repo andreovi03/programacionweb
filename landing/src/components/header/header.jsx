ee4aac190f9ac7207cd9f663e0212fe382121b5f
Andrea Oviedo <andreaoviedor@gmail.com>
2024-02-26 16:28:47 -0500

    burger menu works

--- landing/src/components/header/header.jsx
+++ landing/src/components/header/header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Logo } from './logo/logo'
 import { OptionCards } from './cards/cards'
+import { LINKS } from '../../const/links'
 
 export function Header () {
+  const [showMenu, setShowMenu] = useState(false)
+
+  console.log('state value:', showMenu)
+
+  const handleShowMenu = (event) => {
+    setShowMenu(!showMenu)
+  }
   return (
     <main>
       <div id='header'>
-        <img id='hamburger' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png' />
+        <button onClick={handleShowMenu} className='burguer-menu'>
+          <img id='hamburger' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png' />
+        </button>
         <Logo />
         <div id='searchbar'>
           <h3 id='searchtxt'>Search</h3>
@@ -30,6 +40,35 @@ export function Header () {
         <OptionCards text='APPAREL' iconUrl='https://poppyplaytime.com/cdn/shop/files/apparel-01_250x.svg?v=1688073138' />
         <OptionCards text='ACCESORIES' iconUrl='https://poppyplaytime.com/cdn/shop/files/mug_v2-01_250x.svg?v=1688075839' />
       </div>
+      <Menu
+        items={LINKS}
+        showMenu={showMenu}
+      />
     </main>
+  )
+}
+
+function Menu ({ items, showMenu }) {
+  return (
+    <nav className={!showMenu ? 'hidden' : ''}>
+      <ul>
+        {
+            items.map((item) =>
+              <MenuItem
+                item={item}
+                key={item.id}
+              />)
+      }
+      </ul>
+    </nav>
+  )
+}
+
+function MenuItem ({ item }) {
+  const { text } = item
+  return (
+    <li>
+      <a href=''>{text}</a>
+    </li>
   )
 }
---
+++ landing/src/const/links.js
@@ -0,0 +1,18 @@
+export const LINKS = [
+  {
+    id: crypto.randomUUID(),
+    text: 'New Arrivals'
+  },
+  {
+    id: crypto.randomUUID(),
+    text: 'Plush'
+  },
+  {
+    id: crypto.randomUUID(),
+    text: 'Apparel'
+  },
+  {
+    id: crypto.randomUUID(),
+    text: 'Accessories'
+  }
+]
